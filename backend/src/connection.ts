7f6109dee81fba74416a076f7707b95ec9e77a63
Yadavendra Sakharkar <prajwal1619@gmail.com>
2024-10-10 02:28:44 +0530

    mongo connection

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 const connectToMongoDB = async (url: string): Promise<typeof mongoose> => {
   try {
     const conn = await mongoose.connect(url);
+    console.log("Connection successful!");
     return conn;
   } catch (err) {
+    console.log("Connection failed!");
     console.error(err);
     throw err;
   }
 };
 
-module.exports = { connectToMongoDB };
+export default connectToMongoDB;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express, { Application } from "express";
 import mongoose from "mongoose";
-const connectToMongoDB   = require("./connection");
+import connectToMongoDB from "./connection";
+import * as dotenv from "dotenv";
+dotenv.config();
 const app: Application = express();
-const PORT = process.env.PORT || 5000;
-
-connectToMongoDB(process.env.MONGODB_URL);
+const PORT = process.env.PORT || 3000;
+const url: string = process.env.MONGODB_URL || "";
+console.log(url);
+connectToMongoDB(url);
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
 });
